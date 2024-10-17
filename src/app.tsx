233428ce575bdc0ab83f862aad79639c35c40dea
Lucas Diaz <lucasgustavodiaz@hotmail.com>
2024-10-17 11:23:59 -0300

    feat(ui): display markdown content as plain text

--- src/app.tsx
+++ src/app.tsx
@@ -1,44 +1,13 @@
-import { create } from 'zustand'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './app.css'
 import { Container } from './atoms/container'
 import { MainContent } from './components/main-contenet'
-
-interface CounterState {
-  count: number
-  increment: () => void
-}
-
-const useCounterStore = create<CounterState>(set => ({
-  count: 0,
-  increment: () => set(state => ({ count: state.count + 1 }))
-}))
+import { MarkdownView } from './components/markdown-view'
 
 function App() {
-  const { count, increment } = useCounterStore()
-
   return (
     <Container>
       <MainContent>
-        <div>
-          <a href="https://vitejs.dev" target="_blank">
-            <img src={viteLogo} className="logo" alt="Vite logo" />
-          </a>
-          <a href="https://react.dev" target="_blank">
-            <img src={reactLogo} className="logo react" alt="React logo" />
-          </a>
-        </div>
-        <h1>Vite + React</h1>
-        <div className="card">
-          <button onClick={increment}>count is {count}</button>
-          <p>
-            Edit <code>src/App.tsx</code> and save to test HMR
-          </p>
-        </div>
-        <p className="read-the-docs">
-          Click on the Vite and React logos to learn more
-        </p>
+        <MarkdownView />
       </MainContent>
     </Container>
   )
